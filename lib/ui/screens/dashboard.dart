800e0cabc14ac53cc6a98863aef5913225f8a318
Leonardo Silva <leonardosilvaif@hotmail.com>
2025-12-21 15:10:31 -0300

    feat: new dashboard screen with a bottomNavigationBar

---
+++ lib/ui/screens/dashboard.dart
@@ -0,0 +1,39 @@
+import 'package:cinetopia/ui/screens/releases.dart';
+import 'package:cinetopia/ui/screens/search_movies.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = <Widget>[SearchMovies(), Releases()];
+
+  void updateIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value) => updateIndex(value),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.movie), label: "Filmes"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.today),
+            label: "Lançamentos",
+          ),
+        ],
+      ),
+    );
+  }
+}
