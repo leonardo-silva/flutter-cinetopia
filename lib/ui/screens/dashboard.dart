063c8dbd4280d6e11b7f07a0457bb51ed8dfc7c6
Leonardo Silva <leonardosilvaif@hotmail.com>
2025-12-21 15:48:47 -0300

    feat: using a CustomScrollView to show the list of MovieCards

--- lib/ui/screens/dashboard.dart
+++ lib/ui/screens/dashboard.dart
@@ -22,7 +22,10 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens.elementAt(_currentIndex),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 54, 16, 0),
+        child: _screens.elementAt(_currentIndex),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (value) => updateIndex(value),
--- lib/ui/screens/search_movies.dart
+++ lib/ui/screens/search_movies.dart
@@ -6,6 +6,20 @@ class SearchMovies extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: MovieCard());
+    // In this case we are going to use the widget CustomScrollView instead of SingleChildScrollView
+    // because we cannot use SingleChildScrollView with a Column as a child, it raises an exception.
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverToBoxAdapter(
+          child: Image.asset("assets/movie.png", height: 80, width: 80),
+        ),
+        SliverToBoxAdapter(child: Text("Filmes populares")),
+        SliverToBoxAdapter(child: TextField()),
+        SliverList.builder(
+          itemBuilder: (context, index) => MovieCard(),
+          itemCount: 10,
+        ),
+      ],
+    );
   }
 }
