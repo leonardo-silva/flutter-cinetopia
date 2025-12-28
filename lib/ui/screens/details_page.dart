465d16270e27abd5fc37dfe5e0db551204b79a04
Leonardo Silva <leonardosilvaif@hotmail.com>
2025-12-28 10:08:24 -0300

    refactor: small fixes

--- lib/ui/screens/details_page.dart
+++ lib/ui/screens/details_page.dart
@@ -11,59 +11,56 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent),
       backgroundColor: Color(0xFF1D0E44),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(16, 40, 16, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            Center(
-              child: Column(
-                children: <Widget>[
-                  Image.asset("assets/movie.png", height: 80, width: 80),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 20),
-                    child: Text(
-                      movie.title,
-                      style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.w700,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset("assets/movie.png", height: 80, width: 80),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 20),
+                      child: Text(
+                        movie.title,
+                        style: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    width: 176,
-                    height: 235,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF000000),
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                        image: NetworkImage(imageUrlPrefix + movie.imageUrl),
-                        fit: BoxFit.cover,
+                    Container(
+                      width: 176,
+                      height: 235,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF000000),
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                          image: NetworkImage(imageUrlPrefix + movie.imageUrl),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30, bottom: 15),
-              child: Text(
-                "Description",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 15),
+                child: Text(
+                  "Description",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
               ),
-            ),
-            Text(
-              limitString(movie.overview, 290),
-              style: TextStyle(fontSize: 14),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Text(movie.overview, style: TextStyle(fontSize: 14)),
+              ),
+            ],
+          ),
         ),
       ),
     );
